ed570159f18236c79446fd13349af10ba264dd96
adeo7 <bdmosquera7@misena.edu.co>
2023-10-17 00:02:33 -0500

    habia un error en el repositorio de profesoresMateriasGRados, ya quedo funcioando

--- src/main/java/com/sena/adso2499719/adso2499719/repositories/ProfesorMateriaGradoRepository.java
+++ src/main/java/com/sena/adso2499719/adso2499719/repositories/ProfesorMateriaGradoRepository.java
@@ -13,14 +13,13 @@ import com.sena.adso2499719.adso2499719.entities.ProfesorMateriaGrado;
 public interface ProfesorMateriaGradoRepository extends JpaRepository<ProfesorMateriaGrado, Long>{
 	
 	@Query(value="SELECT \r\n"
-			+ "id,\r\n"
-			+ "mt.nombre AS materia,\r\n"
-			+ "g.nombre AS grado\r\n"
-			+ " FROM adso2499719.aprendices;\r\n"
-			+ "SELECT pmg.id FROM profesores_materias_grados AS pmg\r\n"
-			+ "INNER JOIN materias mt ON mt.id= pmg.materia_id\r\n"
-			+ "INNER JOIN grados g ON g.id =pmg.grado_id\r\n"
-			+ "WHERE pmg.profesor_id= :profesorId", nativeQuery = true)
+			+ "			pmg.id,\r\n"
+			+ "			mt.nombre AS materia,\r\n"
+			+ "			g.nombre AS grado\r\n"
+			+ "            FROM profesores_materias_grados AS pmg\r\n"
+			+ "			INNER JOIN materias mt ON mt.id= pmg.materia_id\r\n"
+			+ "			INNER JOIN grados g ON g.id =pmg.grado_id\r\n"
+			+ "            WHERE pmg.profesor_id= :profesorId", nativeQuery = true)
 	List<ProfesorMateriaGradoDto>getData(Long profesorId);
 	
 }
